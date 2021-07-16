ed5359e66d9aa453cb8520400fbfdb29bef129a6
Reinaldo <reinaldo.riant@kompas.com>
2021-07-16 08:29:47 +0700

    ! integrate to content view

--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -11,7 +11,38 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        Text("1")
+        ScrollView(.vertical, showsIndicators: false){
+            VStack(alignment: .center, spacing: 20){
+                
+                //MARK : - Header
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                //MARK : - Footer
+                
+                VStack(alignment: .center, spacing: 20){
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding()
+                    
+                    Text("Everything you wanted to know about avocados but were to afraid to ask.")
+                        .font(.system(.title, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .padding(0)
     }
 }
 
